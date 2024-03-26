5fefaa5145970d2dd4b3c809f4da515dbb56fafd
zeeshanriasatpl <zeehan.riasat@planlabsolutions.com>
2024-03-26 14:19:53 +0500

    socket added

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const route = require("./src/routes/index");
 const app = express();
 const http = require("http");
 const socketIo = require("socket.io");
-// const server = http.createServer(app);
-// const io = socketIo(server);
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const port = process.env.PORT;
 app.use(express.json());
 
-app.use(route);
+// app.use("/api",route);
+app.use("/api", require("./src/routes"));
 
 app.get("/ping", async (req, res) => {
   res.status(200).json({ message: "Connected" });
@@ -19,14 +20,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
  });
 
-// module.exports = { io, http, server, socketIo };
-// const socket = require("./src/services/socket.service");
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+module.exports = { io, http, server, socketIo };
+const socket = require("./src/services/socket.service");
 
 // app.listen(port, () => {
 //   console.log(`Server is running at http://localhost:${port}`);
 // });
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
