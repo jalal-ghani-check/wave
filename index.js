8e1230266c020d13faffe14114029ac628e3387c
zeeshanriasatpl <zeehan.riasat@planlabsolutions.com>
2024-03-26 14:10:56 +0500

    deploy testing

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const route = require("./src/routes/index");
 const app = express();
 const http = require("http");
 const socketIo = require("socket.io");
-const server = http.createServer(app);
-const io = socketIo(server);
+// const server = http.createServer(app);
+// const io = socketIo(server);
 
 const port = process.env.PORT;
 app.use(express.json());
@@ -15,13 +15,18 @@ app.get("/ping", async (req, res) => {
   res.status(200).json({ message: "Connected" });
 });
 
-module.exports = { io, http, server, socketIo };
-const socket = require("./src/services/socket.service");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+ });
+
+// module.exports = { io, http, server, socketIo };
+// const socket = require("./src/services/socket.service");
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
 // app.listen(port, () => {
 //   console.log(`Server is running at http://localhost:${port}`);
 // });
 
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
