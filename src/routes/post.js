86492a78ed0051a8dce178fb83d87a0d4b253fc0
Abdullah <se.abdullahcheema6@gmail.com>
2024-05-07 18:04:51 +0500

    radius api changed to post

--- src/routes/post.js
+++ src/routes/post.js
@@ -6,7 +6,7 @@ const tokenMiddleware = require("../middlewares/token.user.middleware");
 router.post("/create", tokenMiddleware, post.addPost);
 router.get("/user", tokenMiddleware, post.getPostsByUserId);
 router.put("/update/:id", tokenMiddleware, post.updatePost);
-router.get("/radius", post.specificDistance);
+router.post("/radius", post.specificDistance);
 router.get("/category/:id", post.getPostsByCategoryId);
 router.get("/all", post.allPosts);
 router.get("/:id", post.SpecificPost);
